fe5b523355b7e091c5889176467a44e0d8e24299
miiton <468745+miiton@users.noreply.github.com>
2019-09-07 17:03:07 +0900

    KANZEN NI RIKAI SHITA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
   return (
@@ -19,8 +19,44 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+const initialState = { count: 0 }
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
+
+const Counter: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(
+    () => {
+      document.title = `${state.count}`
+      return () => {
+        document.title = 'hoge'
+      }
+    },
+    [state.count],
+  )
+
+  return (
+    <>
+      <p>You clicked {state.count} times</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  )
+}
+
+export default App
